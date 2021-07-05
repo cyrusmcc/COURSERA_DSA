264424d6797f17697a2963ea9daba7f2fdf7a4fd
cyrusmcc <cyrusmcc@hotmail.com>
2021-07-04 23:17:39 -0600

    isEqual method

--- 8Puzzle/Board.java
+++ 8Puzzle/Board.java
@@ -88,7 +88,18 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return false;
+
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+
+        Board that = (Board) y;
+
+        if(that.dim != this.dim) return false;
+        for(int i = 0; i < n; i++) {
+            if(that.board[i] != this.board[i]) return false;
+        }
+        return true;
     }
 
     // all neighboring boards
@@ -105,15 +116,16 @@ public class Board {
     public static void main(String[] args) {
 
         int[][] tiles = new int[3][3];
+        int[][] tiles2 = new int[3][3];
         int c = 1;
 
-        /*
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
-                tiles[i][j] = c++;
+                tiles[i][j] = c;
+                tiles2[i][j] = c;
+                c++;
             }
         }
-        */
 
         /*
         tiles[0][0] = 8;
@@ -125,13 +137,14 @@ public class Board {
         tiles[2][0] = 7;
         tiles[2][1] = 6;
         tiles[2][2] = 5;
-        */
+         */
 
         Board board = new Board(tiles);
+        Board board2 = new Board(tiles2);
         System.out.println(board.toString());
         System.out.println("Hamming: " + board.hamming());
         System.out.println("Manhattan: " + board.manhattan());
         System.out.println("Is Goal: " + board.isGoal());
-
+        System.out.println("Are equal: " + board.equals(board2));
     }
 }
